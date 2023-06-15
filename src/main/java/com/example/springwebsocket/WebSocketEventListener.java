35e6356315d1b583f5451e7f3bc368b49d8efe80
nikhil <gogulanikhil@gmail.com>
2023-06-15 23:02:03 +0530

    added html

--- src/main/java/com/example/springwebsocket/WebSocketEventListener.java
+++ src/main/java/com/example/springwebsocket/WebSocketEventListener.java
@@ -5,6 +5,7 @@ import com.example.springwebsocket.BaseEntity.MessageType;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
@@ -13,6 +14,7 @@ import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 @RequiredArgsConstructor
 @Slf4j
 public class WebSocketEventListener {
+    private final SimpMessageSendingOperations simpMessageSendingOperations;
    @EventListener
     public void handleWebSocketDisconnectListener(SessionDisconnectEvent sessionDisconnectEvent){
        StompHeaderAccessor stompHeaderAccessor=StompHeaderAccessor.wrap(sessionDisconnectEvent.getMessage());
@@ -22,7 +24,10 @@ public class WebSocketEventListener {
            log.info("user disconnectd : { }",username);
            var chatMessage = ChatMessage.builder()
                    .type(MessageType.LEAVE)
+                   .sender(username)
                    .build();
+           simpMessageSendingOperations.convertAndSend("/topic/public",chatMessage);
+
        }
 
     }
